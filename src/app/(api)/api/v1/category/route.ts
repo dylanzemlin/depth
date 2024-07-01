4af2a37639c5b4c5479446ebb60706430a1410a4
Dylan Zemlin <dylan.zemlin@ou.edu>
2024-07-01 17:01:04 -0500

    Start work on category creation api route

---
+++ src/app/(api)/api/v1/category/route.ts
@@ -0,0 +1,39 @@
+import { withSessionRoute } from "@/lib/iron/wrappers";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { object, string } from "yup";
+
+const schema = object({
+    title: string().required(),
+    description: string().required()
+})
+
+export async function POST(request: NextRequest): Promise<NextResponse>
+{
+    const session = await withSessionRoute();
+    if (session.user == null)
+    {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
+        const { title, description } = await schema.validate(await request.json());
+
+        // Create category
+        // TODO: FIX
+        const category = await prisma.category.create({
+            data: {
+                name: title,
+                user: {
+                    connect: {
+                        id: session.user.id
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(category, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 400 });
+    }
+}
