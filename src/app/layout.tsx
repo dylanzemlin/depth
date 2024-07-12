a8a635c7847a037f30649bbe1001572228037f82
Dylan Zemlin <dylansmrw@gmail.com>
2024-07-12 16:51:40 -0500

    Add subset to roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const robot = Roboto({
     weight: ["400", "500", "700"],
+    subsets: ["latin"]
 });
 import "./globals.css";
 
@@ -30,14 +31,14 @@ export default function RootLayout({
     return (
         <html lang="en light" style={{ colorScheme: "light" }}>
             <body className={`${robot.className} flex flex-col xl:flex-row overflow-y-auto scroll-auto antialiased`}>
-                <QueryClientProvider client={queryClient}>
+                {/* <QueryClientProvider client={queryClient}> */}
                     <AuthProvider>
                         {children}
 
                         <script src="/3rd/flowbite.min.js" />
                         <Toaster position="top-center" />
                     </AuthProvider>
-                </QueryClientProvider>
+                {/* </QueryClientProvider> */}
             </body>
         </html>
     );
