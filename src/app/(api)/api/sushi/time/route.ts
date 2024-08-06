d2b29bbf583585e47a3b6e65503b6d15b3d2bcc1
Dylan Zemlin <dylansmrw@gmail.com>
2024-08-05 21:23:35 -0500

    More timezone fixes

--- src/app/(api)/api/sushi/time/route.ts
+++ src/app/(api)/api/sushi/time/route.ts
@@ -20,15 +20,17 @@ export async function GET(request: NextRequest): Promise<NextResponse>
     const serverNow = dayjs().tz(serverTz);
     const userNow = dayjs().tz(session.user.timezone);
 
-    const isUserAfter = userNow.isAfter(serverNow);
-    const isUserBefore = userNow.isBefore(serverNow);
-    const isUserEqual = userNow.isSame(serverNow);
-    const diff = userNow.diff(serverNow, 'minute');
+    const isUserAfter = userNow.utc().isAfter(serverNow.utc(), "minute");
+    const isUserBefore = userNow.utc().isBefore(serverNow.utc(), "minute");
+    const isUserEqual = userNow.utc().isSame(serverNow.utc(), "minute");
+    const diff = userNow.diff(serverNow, 'hour');
 
     return NextResponse.json({
         serverNow: serverNow.format('YYYY-MM-DD HH:mm:ss'),
+        serverNowUtc: serverNow.utc().format('YYYY-MM-DD HH:mm:ss'),
         serverTz: serverTz,
         userNow: userNow.format('YYYY-MM-DD HH:mm:ss'),
+        userNowUtc: userNow.utc().format('YYYY-MM-DD HH:mm:ss'),
         userTz: session.user.timezone,
         other: {
             isUserAfter: isUserAfter,
